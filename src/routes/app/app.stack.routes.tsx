08ecae66ae579a1022405d1a9888b1ffcebf5c79
mayromyller <deehmyller11@gmail.com>
2025-01-16 23:31:04 -0300

    feat: add new HomeScreen and PurchaseDetailsScreen components

--- src/routes/app/app.stack.routes.tsx
+++ src/routes/app/app.stack.routes.tsx
@@ -3,20 +3,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import {
 	AmountSpentScreen,
 	ContactUserScreen,
+	HomeScreen,
+	PurchaseDetailsScreen,
 	RegisterUserScreen,
 	StampEarnScreen
 } from '@/screens'
 
 export type AppStackParamList = {
+	HomeScreen: undefined
 	AmountSpentScreen: undefined
 	ContactUserScreen: {
 		value: number
 	}
 	RegisterUserScreen: {
 		totalStamps: number
+		value: number
 	}
 	StampEarnScreen: {
 		totalStamps: number
+		value: number
+	}
+	PurchaseDetailsScreen: {
+		value: number
 	}
 }
 
@@ -26,15 +34,20 @@ export function AppStackRoutes() {
 	return (
 		<Stack.Navigator
 			id={undefined}
-			initialRouteName="AmountSpentScreen"
+			initialRouteName="HomeScreen"
 			screenOptions={{
 				headerShown: false
 			}}
 		>
+			<Stack.Screen name="HomeScreen" component={HomeScreen} />
 			<Stack.Screen name="AmountSpentScreen" component={AmountSpentScreen} />
 			<Stack.Screen name="ContactUserScreen" component={ContactUserScreen} />
 			<Stack.Screen name="RegisterUserScreen" component={RegisterUserScreen} />
 			<Stack.Screen name="StampEarnScreen" component={StampEarnScreen} />
+			<Stack.Screen
+				name="PurchaseDetailsScreen"
+				component={PurchaseDetailsScreen}
+			/>
 		</Stack.Navigator>
 	)
 }
