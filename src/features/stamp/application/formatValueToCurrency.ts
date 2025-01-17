8a7e06747016327c7135d97da414251f23b0b6d6
mayromyller <deehmyller11@gmail.com>
2025-01-16 23:38:04 -0300

    fix(formatValueToCurrency): handle null or undefined values

--- src/features/stamp/application/formatValueToCurrency.ts
+++ src/features/stamp/application/formatValueToCurrency.ts
@@ -1,5 +1,5 @@
 export function formatValueToCurrency(value: string | number): string {
-	const valueToNumber = typeof value === 'string' ? Number(value) : value
+	const valueToNumber = typeof value === 'string' ? Number(value) : value || 0
 
 	return valueToNumber.toLocaleString('pt-BR', {
 		style: 'currency',
